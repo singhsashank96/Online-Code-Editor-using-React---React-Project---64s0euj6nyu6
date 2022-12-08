727aba4df23efe9876d8170b438dec1276fd6251
sashank singh <singhsashank96@gmail.com>
2022-12-08 16:10:46 +0530

    added code Editor

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import { Editor } from './Editor';
+import { UseLocalStorage } from './UseLocalStorage';
+
+function App() {
+  
+  const[html, setHtml] = UseLocalStorage('html', '');
+  const[css, setCss] = UseLocalStorage('css', '');
+  const[js, setJs] = UseLocalStorage('js', '');
+  const[srcDoc, setSrcDoc] = useState('');
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+        </html>
+      `)
+    }, 250)
+    return () => clearTimeout(timeout)
+  }, [html, css, js])
+
+
+  return (
+    <>
+    <div className="pane top-pane">
+        <Editor language='xml' displayName='HTML'
+        value={html} onChange={setHtml}/>
+
+        <Editor 
+        language='css' displayName='CSS'
+        value={css} onChange={setCss}/>
+
+        <Editor language='javascript' displayName='JS'
+        value={js} onChange={setJs}/>
+    </div>
+
+    <div className="bottom-pane">
+       <iframe
+          srcDoc={srcDoc}
+          title = 'output'
+//           sandbox = 'allow-script'
+          frameBorder = '0'
+          width = '100%'
+          height = '100%'
+        ></iframe>
+    </div>
+
+    </>
+  );
+}
+
+export default App;
